22c18ab9ecb3a5b804ffa0a23114fb2a58f609c5
chrystalio <chrystalio.k@outlook.com>
2026-02-09 03:02:31 +0700

    feat(ui): adjust grid cols

--- resources/js/pages/bookmarks/index.tsx
+++ resources/js/pages/bookmarks/index.tsx
@@ -535,7 +535,7 @@ export default function BookmarksIndex({
                         </p>
                     </div>
                 ) : (
-                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
                         {bookmarks.data.map((bookmark) => (
                             <div
                                 key={bookmark.id}
